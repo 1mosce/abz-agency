27b9a5353c3f223b88fb7fe1c7855d0014f33516
1mosce <shuliaknikolay@gmail.com>
2022-03-29 12:42:16 +0300

    Project commit

---
+++ cpm/src/js/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/app.scss"
+import Header from "./Components/Header";
+
+function App() {
+    return(
+        <div className="container">
+            <Header/>
+        </div>
+    )
+}
+
+export default App
---
+++ cpm/src/js/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header(){
+    return(
+        <div className="header_wraper">
+            <div></div>
+        </div>
+    )
+}
+
+export default Header
